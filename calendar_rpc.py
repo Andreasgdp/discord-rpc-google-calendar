49a66acf56fdd1e9a8d2b2381a525cf41efb31f9
Andreas <andreasgdp@gmail.com>
2020-01-20 16:34:09 +0100

    Stuff

--- calendar_rpc.py
+++ calendar_rpc.py
@@ -7,6 +7,6 @@ RPC = Presence(client_id)  # Initialize the Presence client
 RPC.connect()  # Start the handshake loop
 
 while True:  # The presence will stay on as long as the program is running
-    RPC.update(state="Rich Presence using pypresence!")  # Updates our presence
+    RPC.update(state="Rich Presence using pypresence!", instance=True)  # Updates our presence
     time.sleep(15)  # Can only update rich presence every 15 seconds
 
